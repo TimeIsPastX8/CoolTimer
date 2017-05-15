df98038ac591e85a890644fafde9c5d787be476a
user <user@DESKTOP-20FNN9M>
2017-05-15 23:58:34 +0900

    Clean Up!

--- Alarm/test/testNoise/testAlarm.java
+++ Alarm/test/testNoise/testAlarm.java
@@ -1,24 +1,12 @@
 package testNoise;
 
-import java.io.FileInputStream;
-
-import javazoom.jl.player.Player;
+import Info.JFramInfo;
 
 public class testAlarm {
 
 	public static void main(String[] args){
 		
-//		JFramInfo frame	= new JFramInfo();
+		JFramInfo frame	= new JFramInfo();
 		
-		String 	path = "C://WORK//makePro//alarm//mp3//bELL.mp3";
-		System.out.println(path);
-		
-		try { 
-			FileInputStream fis = new FileInputStream(path); 
-			Player playMp3 = new Player(fis); 
-			playMp3.play(); 
-		} catch (Exception e) { 
-			System.out.println(e); 
-		}
 	}
 }
